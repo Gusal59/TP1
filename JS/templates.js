053b26d3275f10011b6276971eb641a445b28e84
leonardo <leohtai@gmail.com>
2023-06-30 14:30:12 -0300

    Agregado templates

---
+++ JS/templates.js
@@ -0,0 +1,35 @@
+document.getElementById("idheader").innerHTML = `
+    <div class="div-logo">
+        <a title="Comidas Argentinas" href="index.html"><img src="IMG/Logo2.png" alt="Comidas Argentinas" class="logo" /></a>
+    </div>
+    <div class="div-tituloPrincipal">
+        <section class="palabras palabra-1">
+            <span>Comidas</span>
+        </section>
+        <section class="palabras palabra-2">
+            <span>Regionales</span>
+        </section>
+        <section class="palabras palabra-3">
+            <span>Argentinas</span>
+        </section>
+    </div>  
+`
+
+document.getElementById("idnavegacion").innerHTML = `
+<a href="noroeste.html" class="item item1">Noroeste</a>
+<a href="nordeste.html" class="item item2">Nordeste</a>
+<a href="pampa.html" class="item item3">Pampeana</a>
+<a href="cuyo.html" class="item item4">Cuyo</a>
+<a href="patagonia.html" class="item item5">Patagonia</a>`
+
+document.getElementById("idfooter").innerHTML = `
+<div class="footer-link">
+<H4 class="seguinos">Seguinos:</H4>
+<div class="social-link">
+    <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin"></i></a>
+</div>
+<a href="contactoFormspree.html"><H4 class="contacto">Contacto</H4></a>
+</div>`
